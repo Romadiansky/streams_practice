5c6199bf4d16b4a0c0f518d909c942f91bdafbf6
romadiansky <letsshitrainbows@gmail.com>
2018-09-30 16:07:10 +0000

    chunk requested

--- https_example.js
+++ https_example.js
@@ -7,17 +7,22 @@ var options = {
   path: '/'
 };
 
-var callback = function() {
+var callback = function(response) {
   console.log('In response handler callback!');
+
+  response.on('data', function(chunk) {
+    console.log('[-- CHUNK OF LENGTH ' + chunk.length + ' --]');
+    console.log(chunk.toString());
+  });
 }
 
 console.log("I'm about to make the request!");
 
 console.log("I've made the request!");
 
-var callback = function(response) {
-  console.log('In response handler callback!');
-  console.log('Response: ', response);
-}
+// var callback = function(response) {
+//   console.log('In response handler callback!');
+//   console.log('Response: ', response);
+// }
 
 https.request(options, callback).end();
