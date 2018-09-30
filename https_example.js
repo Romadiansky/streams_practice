94afed0d49305b3cde7d27f6c6230e1b75bc75f6
romadiansky <letsshitrainbows@gmail.com>
2018-09-30 15:27:20 +0000

    requested incoming message!

--- https_example.js
+++ https_example.js
@@ -13,6 +13,11 @@ var callback = function() {
 
 console.log("I'm about to make the request!");
 
-https.request(options, callback).end();
-
 console.log("I've made the request!");
+
+var callback = function(response) {
+  console.log('In response handler callback!');
+  console.log('Response: ', response);
+}
+
+https.request(options, callback).end();
